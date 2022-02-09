8e7f841151752636d8a40f0c0b0787becf176e29
Adam Callahan <47254638+AdamCallahan01@users.noreply.github.com>
2022-02-09 13:16:30 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          The Future platform of Wizard Chess, a program being developed by Adam and Austin
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.whitworth.edu/cms/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Whitworth University
         </a>
       </header>
     </div>
